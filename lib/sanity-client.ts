9d45fd0b6f7d51664209740128d3d04f51fd820a
amithkm9 <amithkm9@gmail.com>
2025-04-26 12:45:13 +0530

    debugging blog posts

--- lib/sanity-client.ts
+++ lib/sanity-client.ts
@@ -11,7 +11,7 @@ export const config = {
   projectId: 'dz88krr6',
   dataset: 'production',
   apiVersion: '2023-05-03', // Use a UTC date string
-  useCdn: process.env.NODE_ENV === 'production', // Use CDN for faster response in production
+  useCdn: false, // Use CDN for faster response in production
 };
 
 // Create a client for fetching data
